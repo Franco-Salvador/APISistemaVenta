5546b199a9f315d09357b69adb2cd1583ce9ed87
Franco Antonio Salvador Torres <fsalvadort1@upao.edu.pe>
2025-01-11 11:09:32 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<Tmodel>where Tmodel : class 
+    {
+        Task<Tmodel> Obtener(Expression<Func<Tmodel,bool>>filtro);
+        Task<Tmodel> Crear(Tmodel modelo);
+        Task<bool> Editar(Tmodel modelo);
+        Task<bool> Eliminar(Tmodel modelo); 
+        Task<IQueryable<Tmodel>>Consultar(Expression<Func<Tmodel, bool>> filtro = null);   
+    }
+}   
